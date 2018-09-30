d3127da9577eddc6bde8c1ea7387f2de2c09b5bf
kesavakumar <kesavakumar@crimsonlogic.com>
2018-09-30 15:14:47 +0530

    Base templates

---
+++ src/main/java/com/sfg/petshop/SguruPetExampleApplication.java
@@ -0,0 +1,12 @@
+package com.sfg.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguruPetExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SguruPetExampleApplication.class, args);
+	}
+}
